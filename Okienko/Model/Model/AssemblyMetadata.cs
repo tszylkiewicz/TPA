f3ab7ee23b85a80bd7a2cfcb2423b07c0b303516
tszylkiewicz <43911837+tszylkiewicz@users.noreply.github.com>
2019-01-17 00:56:22 +0100

    Poprawa nazw projektów

--- Okienko/Model/Model/AssemblyMetadata.cs
+++ Okienko/Model/Model/AssemblyMetadata.cs
@@ -21,7 +21,6 @@ namespace Model.Model
         {
             this.Name = assembly.ManifestModule.Name;
             this.Namespaces = (from Type _type in assembly.GetTypes()
-                           where _type.GetVisible()
                            group _type by _type.GetNamespace() into _group
                            orderby _group.Key
                            select new NamespaceMetadata(_group.Key, _group.ToList())).ToList();
